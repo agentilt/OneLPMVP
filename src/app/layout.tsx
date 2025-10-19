70d004f2efde39e8ac3b8695f81ef1e363f59780
Agustin Gentil <99360440+agentilt@users.noreply.github.com>
2025-10-19 18:58:40 +0200

    vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Providers } from '@/components/Providers'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -38,6 +39,7 @@ export default function RootLayout({
       </head>
       <body>
         <Providers>{children}</Providers>
+        <SpeedInsights />
       </body>
     </html>
   )
