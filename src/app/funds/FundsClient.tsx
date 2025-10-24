460aa0f887f1bb582028cc6433f7d0b7b44b2013
Agustin Gentil <99360440+agentilt@users.noreply.github.com>
2025-10-24 19:14:47 +0200

    build fix

--- src/app/funds/FundsClient.tsx
+++ src/app/funds/FundsClient.tsx
@@ -12,9 +12,9 @@ interface Fund {
   domicile: string
   vintage: number
   manager: string
-  managerEmail?: string
-  managerPhone?: string
-  managerWebsite?: string
+  managerEmail: string | null
+  managerPhone: string | null
+  managerWebsite: string | null
   commitment: number
   paidIn: number
   nav: number
